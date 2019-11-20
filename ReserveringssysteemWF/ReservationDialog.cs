78fde51524cdb991e77b0b4d509cd7b172bde805
Victor Hemmekam <49402050+VHemmekam@users.noreply.github.com>
2019-11-20 11:06:05 +0100

    Add ReservationDialog

---
+++ ReserveringssysteemWF/ReservationDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReserveringssysteemWF
+{
+    public partial class ReservationDialog : Form
+    {
+        public ReservationDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}

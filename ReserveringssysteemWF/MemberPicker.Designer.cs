7ac02294d2a2df5bf90d6f796ea6db990986b626
Victor Hemmekam <49402050+VHemmekam@users.noreply.github.com>
2019-11-20 12:17:59 +0100

    Update member picker design

--- ReserveringssysteemWF/MemberPicker.Designer.cs
+++ ReserveringssysteemWF/MemberPicker.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.reserveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(481, 278);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Annuleren";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // reserveButton
+            // 
+            this.reserveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.reserveButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.reserveButton.Location = new System.Drawing.Point(400, 278);
+            this.reserveButton.Name = "reserveButton";
+            this.reserveButton.Size = new System.Drawing.Size(75, 23);
+            this.reserveButton.TabIndex = 2;
+            this.reserveButton.Text = "Reserveren";
+            this.reserveButton.UseVisualStyleBackColor = true;
+            // 
+            // MemberPicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.reserveButton);
+            this.Name = "MemberPicker";
             this.Text = "MemberPicker";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button reserveButton;
     }
 }

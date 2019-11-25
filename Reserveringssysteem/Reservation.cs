f79436c7d717c2c36e7d5bd801199c8c28b6be92
Victor Hemmekam <49402050+VHemmekam@users.noreply.github.com>
2019-11-25 12:57:44 +0100

    Update Reservation.cs

--- Reserveringssysteem/Reservation.cs
+++ Reserveringssysteem/Reservation.cs
@@ -24,5 +24,38 @@ namespace Reserveringssysteem
 
         [Required]
         public Boat Boat { get; set; }
+
+        public DateTime[] GetAvailableStartTimes(BoatType boatType, DateTime date, TimeSpan duration)
+        {
+            List<DateTime> startTimes = new List<DateTime>();
+
+            date = date.Date + new TimeSpan(0, 0, 0);
+
+            if (boatType.Boats.Count > 0)
+                // Start enumerating from 12:00 to 17:00 (each 15 minutes)
+                for (DateTime startTime = date.AddHours(12); startTime <= date.AddHours(17); startTime += new TimeSpan(0, 15, 0))
+                {
+                    bool available = false;
+                    DateTime endTime = startTime + duration;
+
+                    foreach (Boat boat in boatType.Boats)
+                    {
+                        if (available) break;
+
+                        foreach (Reservation reservation in boat.Reservations)
+                        {
+                            if (!(startTime <= reservation.DateTime + reservation.Duration && reservation.DateTime <= endTime))
+                            {
+                                available = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (available) startTimes.Add(startTime);
+                }
+
+            return startTimes.ToArray();
+        }
     }
 }

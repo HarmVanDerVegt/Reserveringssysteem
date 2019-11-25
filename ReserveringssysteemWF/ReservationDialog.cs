83256c5d2451175bf786ac652fe20b8bd8a14207
Victor Hemmekam <49402050+VHemmekam@users.noreply.github.com>
2019-11-25 12:27:19 +0100

    Update ReservationDialog.cs

--- ReserveringssysteemWF/ReservationDialog.cs
+++ ReserveringssysteemWF/ReservationDialog.cs
@@ -14,54 +14,12 @@ namespace ReserveringssysteemWF
 {
     public partial class ReservationDialog : Form
     {
-        private List<BoatType> boatTypes = new List<BoatType>();
-        private List<Certificate> certificates = new List<Certificate>();
         private BindingList<Member> members = new BindingList<Member>();
         private BindingList<BoatType> displayBoatTypes = new BindingList<BoatType>();
         private BindingList<Member> coxswains = new BindingList<Member>();
         private BindingList<DateTime> startTimes = new BindingList<DateTime>();
         private BindingList<TimeSpan> durations = new BindingList<TimeSpan>();
 
-        /// <summary>
-        /// Algorithm to show all available boat types based on the team's certificates.
-        /// </summary>
-        private void UpdateDisplayBoatTypes()
-        {
-            displayBoatTypes.Clear();
-
-            foreach (BoatType boatType in boatTypes)
-                // Get boats with the size of the ammount of members in the current team.
-                if (boatType.Size + (boatType.HasCoxswain ? 1 : 0) == members.Count)
-                {
-                    bool available = true;
-
-                    foreach (Member member in members)
-                        // If there is a member that does not have the certificate of the current boat then don't show the current boat.
-                        if (member.Levels == null || !member.Levels.Select(c => c.ID).ToList().Contains(boatType.ID))
-                        {
-                            available = false;
-                            break;
-                        }
-
-                    if (available)
-                        displayBoatTypes.Add(boatType);
-                }
-        }
-
-        /// <summary>
-        /// Algorithm to show all available coxswains based on the team's certificates.
-        /// </summary>
-        private void UpdateCoxswain()
-        {
-            coxswains.Clear();
-            // Get the coxswain certificate.
-            Certificate coxswainCertificate = certificates.Find(c => c.Name == "Certificate Stuurman");
-
-            foreach (Member member in members)
-                // Check if the current member has the coxswain certificate.
-                if (member.Levels != null && coxswainCertificate != null && member.Levels.Select(c => c.ID).ToList().Contains(coxswainCertificate.ID))
-                    coxswains.Add(member);
-        }
 
         /// <summary>
         /// Algorithm to show all available start times based on the boat type boats availability (start time & duration).
@@ -118,12 +76,6 @@ namespace ReserveringssysteemWF
 
         private void UpdateDisplay()
         {
-            // Show available boats based on the team
-            UpdateDisplayBoatTypes();
-
-            // Show available coxswains based on the team
-            UpdateCoxswain();
-
             // Show available starttimes
             UpdateStartTimes();
 
@@ -166,14 +118,6 @@ namespace ReserveringssysteemWF
             durationComboBox.DataSource = durations;
             startTimeComboBox.DataSource = startTimes;
 
-            using (ReserveringssysteemContext context = new ReserveringssysteemContext())
-            {
-                foreach (BoatType boatType in context.BoatTypes.Include("Boats.Reservations"))
-                    boatTypes.Add(boatType);
-
-                foreach (Certificate certificate in context.Certificates)
-                    certificates.Add(certificate);
-            }
 
             datePicker.MinDate = DateTime.Now.AddDays(2);
             datePicker.MaxDate = DateTime.Now.AddDays(14);
@@ -204,22 +148,6 @@ namespace ReserveringssysteemWF
         {
             using (ReserveringssysteemContext context = new ReserveringssysteemContext())
             {
-                Reservation reservation = new Reservation();
-                RecreationalTeam recreationalTeam = new RecreationalTeam();
-                recreationalTeam.Users = new List<User>();
-
-                foreach (Member member in members)
-                    recreationalTeam.Users.Add(context.Members.Find(member.ID));
-
-                if (coxswainComboBox.SelectedItem != null)
-                    recreationalTeam.Coxswain = context.Members.Find(((Member)coxswainComboBox.SelectedItem).ID);
-
-                reservation.Team = recreationalTeam;
-                reservation.Boat = context.Boats.Find(1);
-                reservation.DateTime = (DateTime)startTimeComboBox.SelectedItem;
-                reservation.Duration = (TimeSpan)durationComboBox.SelectedItem;
-
-                context.Reservations.AddOrUpdate(reservation);
                 context.SaveChanges();
             }
         }

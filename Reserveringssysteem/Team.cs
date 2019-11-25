f8c1b14ea0fc16eb889eb32b391f3cde2485902f
Victor Hemmekam <49402050+VHemmekam@users.noreply.github.com>
2019-11-25 12:04:42 +0100

    Update move GetAvailableBoatTypes from BoatType to Team

--- Reserveringssysteem/Team.cs
+++ Reserveringssysteem/Team.cs
@@ -20,5 +20,35 @@ namespace Reserveringssysteem
         public List<User> Users { get; set; }
 
         public List<Match> Matches { get; set; }
+
+        /// <summary>
+        /// Get all available boat types based on a given team's certificates and size.
+        /// </summary>
+        /// <returns>An array of boat types</returns>
+        public BoatType[] GetAvailableBoatTypes()
+        {
+            List<BoatType> boatTypes = new List<BoatType>();
+
+            using (ReserveringssysteemContext context = new ReserveringssysteemContext())
+                foreach (BoatType boatType in context.BoatTypes.Include("Certificate"))
+                    // Check if this boat type has the same size as the team.
+                    if (boatType.Size + (boatType.HasCoxswain ? 1 : 0) == Users.Count)
+                    {
+                        bool available = true;
+
+                        foreach (User user in Users)
+                            // Check if this user contains the certificate of this boat type, if not then this boat is not available.
+                            if (user.Levels == null || !user.Levels.Select(c => c.ID).ToList().Contains(boatType.Certificate.ID))
+                            {
+                                available = false;
+                                break;
+                            }
+
+                        if (available)
+                            boatTypes.Add(boatType);
+                    }
+
+            return boatTypes.ToArray();
+        }
     }
 }
